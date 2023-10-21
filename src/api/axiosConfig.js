541d697c5f3127ff500a1ab7e9cc9d2c2ac8796b
Anubhav9955 <singh.anubhav.98010@gmal.com>
2023-10-21 12:19:57 +0530

    url fixes

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an instance of Axios with a custom configuration
 const instance = axios.create({
-  baseURL: "http://localhost:5000", // Set your base URL here
+  baseURL: "https://mini-stock-price-tracker-api.vercel.app", // Set your base URL here
 });
 
 export default instance;
